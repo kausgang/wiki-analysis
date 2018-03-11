9be55d9593a2b5c3aabe35537a68aae01c4872ea
Kaustav Ganguli <write.to.kaustav@gmail.com>
2018-03-11 15:52:03 -0400

    introduced search input for north pane

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,37 +1,43 @@
 
 
-var myLayout;
+// var myLayout;
 
 $(document).ready(function () {
 
     // this layout could be created with NO OPTIONS - but showing some here just as a sample...
     // myLayout = $('body').layout(); -- syntax with No Options
 
-    myLayout = $('body').layout({
+    $('body').layout({
 
-        //	reference only - these options are NOT required because 'true' is the default
+
+
         closable: true	// pane can open & close
-        , resizable: true	// when open, pane can be resized
-        , slidable: true	// when closed, pane can 'slide' open over other panes - closes on mouse-out
-        , livePaneResizing: true
+
+        // , resizable: true	// when open, pane can be resized
+        // , slidable: true	// when closed, pane can 'slide' open over other panes - closes on mouse-out
+        // , livePaneResizing: true
 
         //	some resizing/toggling settings
-        , north__slidable: false	// OVERRIDE the pane-default of 'slidable=true'
-        , north__togglerLength_closed: '100%'	// toggle-button is full-width of resizer-bar
-        , north__spacing_closed: 20		// big resizer-bar when open (zero height)
-        , south__resizable: true	// OVERRIDE the pane-default of 'resizable=true'
-        , south__spacing_open: 0		// no resizer-bar when open (zero height)
-        , south__spacing_closed: 20		// big resizer-bar when open (zero height)
+        // , north__slidable: false	// OVERRIDE the pane-default of 'slidable=true'
+
+        // , north__togglerLength_closed: '100%'	// toggle-button is full-width of resizer-bar
+        // , north__spacing_closed: 20		// big resizer-bar when open (zero height)
+        // , south__resizable: true	// OVERRIDE the pane-default of 'resizable=true'
+        // , south__spacing_open: 0		// no resizer-bar when open (zero height)
+        // , south__spacing_closed: 20		// big resizer-bar when open (zero height)
 
         //	some pane-size settings
         // , west__minSize: 100
         // , east__size: 350
         // , east__minSize: 200
-        , east__maxSize: .5 // 50% of layout width
         // , center__minWidth: 100
 
 
+
+        , east__maxSize: .5 // 50% of layout width
         , center__minHeight:				200
+
+
 
 
 
@@ -46,11 +52,11 @@ $(document).ready(function () {
         , showDebugMessages: true // log and/or display messages from debugging & testing code
 
 
-        /////ADDED THIS TO HOLD INITIAL SIZE....THIS WILL MESS UP THE DISPLAY IF WINDOW IS RESIZED
-        // , west__size: 350
 
 
     });
+
+
 
 
     $('.ui-layout-east').layout({
@@ -65,7 +71,7 @@ $(document).ready(function () {
 
 
 
-
+    //if url is clicked on west pane
     $(".url1").click(function (e) {
 
       //PREVENT URL FROM LOADING
@@ -86,29 +92,61 @@ $(document).ready(function () {
         })
 
 
+
+
+
+
+});
+
+
+
+
+    //OPERATION ON SEARCH BAR...ENTER WILL SEARCH WIKIPEDIA
+    // capturing the search results after the end user presses (and releases) the ENTER button (keycode 13),
+    $('#search_input').on('keyup', function(e){
+
+
+        if(e.keyCode === 13) {
+
+            //simulate search_button press
+            search_wiki();
+
+        };
+    });
+
+
+
+
+
+
+
+
+
 });
 
 
 
 
 
-});
 
 
 
 
 
+function search_wiki() {
 
-// $(".url1").click(function (e) {
-//
-//     e.preventDefault();
-//
-//     var value = $(this).attr("href");
-//     //alert(value);
-//
-//     $("#level_0_center").html('<object width="100%"  height="100%" data="https://en.wikipedia.org/wiki/India"/ >')
-//
-//
-//
-// });
+    //obtain search string
+    var search_string = $('#search_input').val();
 
+    //reset search field
+    $('#search_input').val('');
+
+
+
+    //FOR TESTING PURPOSE
+    $.get('/testing',function (data) {
+
+        $("#level_1_center").html(data);
+    });
+
+}
